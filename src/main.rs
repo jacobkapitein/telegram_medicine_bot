671e2608f33019ca5c3d561068dff4b0232cd350
Jacob Kapitein <jacob.kapitein@protonmail.com>
2022-04-13 23:43:30 +0200

    Changed a lot in the methods of receiving commands and handling them. Also handle callbacks now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,75 @@
+use std::error::Error;
 use teloxide::prelude2::*;
+use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
+use teloxide::utils::command::BotCommand;
+
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+#[derive(BotCommand, Clone)]
+#[command(rename = "lowercase", description = "The following commands are supported:")]
+enum Command {
+    #[command(description = "shows this message")]
+    Help,
+    #[command(description = "checks if the bot is stil alive")]
+    Ping,
+    #[command(description = "test for an inline keyboard callback")]
+    CallbackTest,
+}
+
+fn make_keyboard(reminder_id: String) -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    let reply_options = ["Yes", "No"];
+    let mut keyboard_row: Vec<InlineKeyboardButton> = vec![];
+
+    for option in reply_options {
+        let button: InlineKeyboardButton = InlineKeyboardButton::callback(option.parse().unwrap(), format!("{}_{}", option, reminder_id));
+        keyboard_row.push(button);
+    }
+    keyboard.push(keyboard_row);
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
+fn random_string() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(24)
+        .map(char::from)
+        .collect()
+}
+
+async fn command_handler(
+    message: Message,
+    command: Command,
+    bot: AutoSend<Bot>
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => bot.send_message(message.chat.id, Command::descriptions()).await?,
+        Command::Ping => bot.send_message(message.chat.id, "Pong!").await?,
+        Command::CallbackTest => {
+            let reminder_id = random_string();
+            let keyboard = make_keyboard(reminder_id);
+            bot.send_message(message.chat.id, "Reminder test").reply_markup(keyboard).await?
+        }
+    };
+
+    Ok(())
+}
+
+async fn callback_handler(
+    query: CallbackQuery,
+    bot: AutoSend<Bot>
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(version) = query.data {
+        println!("{}", version);
+    }
+
+    Ok(())
+}
+
+
 
 // This is the main function to start up the bot
 async fn run() {
@@ -10,56 +81,11 @@ async fn run() {
     // Create bot
     let bot = Bot::from_env().auto_send();
 
-    // Message listener
-    teloxide::repls2::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        match message.text() {
-            Some(text) => {
-                if String::from(text).starts_with('/') {
-                    // Cut off the '/', trim, lowercase it and send for processing
-                    let command: String = text[1..text.len()].trim().to_lowercase();
-                    handle_command(&bot, &message, command.as_str()).await;
-                } else {
-                    bot.send_message(
-                        message.chat.id,
-                        "Sorry, I am not familiar with that command",
-                    )
-                    .await?;
-                }
-            }
-            None => {
-                bot.send_message(message.chat.id, "I can only handle text iput")
-                    .await?;
-            }
-        }
+    let handler = dptree::entry()
+        .branch(Update::filter_message().filter_command::<Command>().endpoint(command_handler))
+        .branch(Update::filter_callback_query().endpoint(callback_handler));
 
-        respond(())
-    })
-    .await;
-}
-
-async fn handle_command(bot: &AutoSend<teloxide::Bot>, message_info: &Message, command: &str) {
-    match command {
-        // Ping command
-        // Check if bot is still online
-        "ping" => match bot.send_message(message_info.chat.id, "Pong!").await {
-            Ok(_result) => {}
-            Err(error) => {
-                log::error!("{}", error)
-            }
-        },
-        // Unknown command
-        _ => {
-            match bot
-                .send_message(message_info.chat.id, "Sorry, I don't know this command")
-                .await
-            {
-                Ok(_result) => {}
-                Err(error) => {
-                    log::error!("{}", error)
-                }
-            }
-        }
-    }
+    Dispatcher::builder(bot, handler).build().setup_ctrlc_handler().dispatch().await;
 }
 
 fn set_token(token: &str) {
